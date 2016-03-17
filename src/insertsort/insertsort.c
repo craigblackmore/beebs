9ba90ce99ef21038a0a67eb1e7c7f2ec515f291f
Craig Blackmore <craig.blackmore@bristol.ac.uk>
2016-03-17 09:28:55 +0000

    init func for 7 bmarks

--- src/insertsort/insertsort.c
+++ src/insertsort/insertsort.c
@@ -68,15 +68,13 @@
    benchmarks. */
 #define SCALE_FACTOR    (REPEAT_FACTOR >> 0)
 
-unsigned int a[11];
+/* assume all data is positive */
+unsigned int a[11] = {0,11,10,9,8,7,6,5,4,3,2};
 
 int
 benchmark (void)
 {
   int i,j, temp;
-  a[0] = 0;   /* assume all data is positive */
-  a[1] = 11; a[2]=10;a[3]=9; a[4]=8; a[5]=7; a[6]=6; a[7]=5;
-  a[8] =4; a[9]=3; a[10]=2;
   i = 2;
   while(i <= 10){
       j = i;
